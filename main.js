3933bd152b033cfdc050bbd7724c44ff09877df1
KRMaendo <40663381+KRMaendo@users.noreply.github.com>
2020-03-26 11:47:30 +0200

    toggle button moves

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-document.getElementById('dark-toggle').onclick();
+document.getElementById('dark-toggle').addEventListener('click', function() {
+	document.querySelector('.little-ball').classList.toggle('toggle');
+	console.log(document.querySelector('.cover'));
+});
